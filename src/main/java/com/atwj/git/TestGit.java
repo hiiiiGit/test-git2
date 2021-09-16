5713d6f51b22d6d7eb303076ffb2900642ea6f1f
hiiiiGit <90864050+hiiiiGit@users.noreply.github.com>
2021-09-17 06:50:38 +0800

    pull commit

--- src/main/java/com/atwj/git/TestGit.java
+++ src/main/java/com/atwj/git/TestGit.java
@@ -18,5 +18,6 @@ public class TestGit {
         System.out.println("hot-fix test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
