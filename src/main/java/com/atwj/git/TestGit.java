ee14deecc593aaaa5cf7cf6bae9e077061867308
Frank <Frank@java.com>
2021-09-17 05:54:50 +0800

    first commit

---
+++ src/main/java/com/atwj/git/TestGit.java
@@ -0,0 +1,13 @@
+package com.atwj.git;
+
+/**
+ * @author shkstart
+ * @create 2021-09-17 05:34
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("haha");
+    }
+}
